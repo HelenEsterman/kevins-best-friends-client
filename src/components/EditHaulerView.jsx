b3408a07b5f384769e6fad1c6ec37fdbbb5fbcd9
Helen Esterman <helenesterman99@gmail.com>
2023-10-27 16:02:12 -0400

    created skeleton of what edit hauler view will look likebut not finished

--- src/components/EditHaulerView.jsx
+++ src/components/EditHaulerView.jsx
@@ -1,6 +1,38 @@
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import { getHaulerByIdWithDock } from "../data/haulerData";
+import { getAllDocks } from "../data/dockData";
 
 export const EditHauler = () => {
+  const [allHaulersWithDocks, setAllHaulersWithDocks] = useState([]);
+  const [allDocks, setAllDocks] = useState([]);
   const { haulerId } = useParams();
-  return <>edit hauler {haulerId}</>;
+
+  useEffect(() => {
+    getHaulerByIdWithDock(haulerId).then((haulerWithDockObj) => {
+      setAllHaulersWithDocks(haulerWithDockObj);
+    });
+    getAllDocks().then((dockArray) => {
+      setAllDocks(dockArray);
+    });
+  }, [haulerId]);
+
+  return (
+    <form>
+      <h1 className="edit-hauler-header text-4xl">Edit Hauler</h1>
+      <fieldset>
+        <label>
+          Hauler Name: <input type="text" name="" />
+        </label>
+      </fieldset>
+      <fieldset>
+        <label>Dock: </label>
+        <select>
+          {allDocks.map((dockObj) => {
+            return <option key={dockObj.id}>{dockObj.location}</option>;
+          })}
+        </select>
+      </fieldset>
+    </form>
+  );
 };
--- src/data/haulerData.jsx
+++ src/data/haulerData.jsx
@@ -1,3 +1,9 @@
-export const getAllHaulers = () =>{
-    return fetch("http://localhost:8000/haulers").then((res)=>res.json())
-}
+export const getAllHaulers = () => {
+  return fetch("http://localhost:8000/haulers").then((res) => res.json());
+};
+
+export const getHaulerByIdWithDock = (haulerId) => {
+  return fetch(`http://localhost:8000/haulers/${haulerId}?_expand=docks`).then(
+    (res) => res.json()
+  );
+};
